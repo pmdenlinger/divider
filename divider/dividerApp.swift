0df3cadacf54db3d0532a339f595228bce5fd288
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:39:24 -0700

    Initial Commit

---
+++ divider/dividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dividerApp.swift
+//  divider
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct dividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
